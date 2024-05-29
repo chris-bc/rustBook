9af663d862471b93c5b3c76b40ad19050e8e2874
Chris Bennetts-Cash <chris@bennettscash.id.au>
2024-05-29 19:02:26 +1000

    further doc styles

--- documentation/src/lib.rs
+++ documentation/src/lib.rs
@@ -1,5 +1,58 @@
+//! # Documentation Crate
+//! 
+//! `documentation` is a collection of simple maths functions to demonstrate
+//! different methods of documentation in Rust
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+/// Adds one to the number given.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = documentation::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+/// # Art
+/// 
+/// A library for modeling artistic concepts.
+
+pub use self::kinds::PrimaryColour;
+pub use self::kinds::SecondaryColour;
+pub use self::utils::mix;
+
+pub mod kinds {
+    /// The primary colours according to the RYB colour model.
+    pub enum PrimaryColour {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colours according to the RYB colour model.
+    pub enum SecondaryColour {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colours in equal amounts to create
+    /// a secondary colour.
+    pub fn mix(c1: PrimaryColour, c2: PrimaryColour) -> SecondaryColour {
+        SecondaryColour::Purple
+    }
 }
 
 #[cfg(test)]
---
+++ documentation/src/main.rs
@@ -0,0 +1,8 @@
+use documentation::PrimaryColour;
+use documentation::mix;
+
+fn main() {
+    let red = PrimaryColour::Red;
+    let yellow = PrimaryColour::Yellow;
+    mix(red, yellow);
+}
