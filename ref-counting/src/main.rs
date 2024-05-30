970deeb94dc9679fcf2ee7eff687f5bedef3923b
Chris Bennetts-Cash <chris@bennettscash.id.au>
2024-05-30 14:43:24 +1000

    Initial ref count examples

--- ref-counting/src/main.rs
+++ ref-counting/src/main.rs
@@ -1,3 +1,19 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Ref count after creating a: {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("Ref count after creating b: {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("Ref count after creating c: {}", Rc::strong_count(&a));
+    }
+    println!("Ref count after c goes out of scope: {}", Rc::strong_count(&a));
 }
