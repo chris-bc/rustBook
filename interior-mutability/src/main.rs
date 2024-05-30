2423f88bcdd7bfdd0c97524763d348a5eedd63d4
Chris Bennetts-Cash <chris@bennettscash.id.au>
2024-05-30 18:24:23 +1000

    Example cyclic dependency

--- interior-mutability/src/main.rs
+++ interior-mutability/src/main.rs
@@ -8,6 +8,21 @@ enum List {
     Nil,
 }
 
+#[derive(Debug)]
+enum ListCycle {
+    Cons(i32, RefCell<Rc<ListCycle>>),
+    Nil,
+}
+
+impl ListCycle {
+    fn tail(&self) -> Option<&RefCell<Rc<ListCycle>>> {
+        match self {
+            ListCycle::Cons(_, item) => Some(item),
+            ListCycle::Nil => None,
+        }
+    }
+}
+
 fn main() {
     let value = Rc::new(RefCell::new(5));
 
@@ -21,4 +36,26 @@ fn main() {
     println!("a after mut: {:?}", a);
     println!("b after mut: {:?}", b);
     println!("c after mut: {:?}", c);
+
+    let a = Rc::new(ListCycle::Cons(5, RefCell::new(Rc::new(ListCycle::Nil))));
+
+    println!("a initial ref count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(ListCycle::Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a ref count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial ref count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b ref count after changing a = {}", Rc::strong_count(&b));
+    println!("a ref count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
