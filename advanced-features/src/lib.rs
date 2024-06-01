ecc02cee0f1065b342141ad5e87bb34df06edf36
Chris Bennetts-Cash <chris@bennettscash.id.au>
2024-06-01 17:56:25 +1000

    Generic types to add metres to millimetres, returning millimetres

--- advanced-features/src/lib.rs
+++ advanced-features/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 struct Counter {
     value: u32,
 }
@@ -11,12 +13,23 @@ impl Iterator for Counter {
     }
 }
 
+struct Millimetres(u32);
+struct Metres(u32);
+
+impl Add<Metres> for Millimetres {
+    type Output = Millimetres;
+
+    fn add(self, other: Metres) -> Millimetres {
+        Millimetres(self.0 + (other.0 * 1000))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn add_metres_to_millimetres() {
+        assert_eq!(Millimetres(500).add(Metres(3)).0, 3500);
     }
 }
